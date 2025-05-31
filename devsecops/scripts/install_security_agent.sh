6d58a185fc9c799acecc1f7adcef5a094a6f2a14
phillrog <8622005+phillrog@users.noreply.github.com>
2025-05-31 17:45:07 -0300

    aws - DevSecOps na AWS

--- devsecops/scripts/install_security_agent.sh
+++ devsecops/scripts/install_security_agent.sh
@@ -2,7 +2,7 @@
 # Created by The Cloud Bootcamp - Install Security Agent Simulation
 
 echo "Downloading security agent..."
-sudo wget -q https://raw.githubusercontent.com/phillrog/aws-projetos/refs/heads/main/devsecops/agent/security_agent -P /usr/bin
+sudo wget -q https://raw.githubusercontent.com/phillrog/aws-projetos/refs/heads/main/devsecops/agent/security_agent -P /usr/bin | bash
 echo "Security agent installation in progress..."
 sudo chmod +x /usr/bin/security_agent
 sleep 30
