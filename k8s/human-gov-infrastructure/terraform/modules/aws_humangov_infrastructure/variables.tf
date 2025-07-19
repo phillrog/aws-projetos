e92799d884c5164f562b7c7ddea8c31608e30fbd
Phillipe <8622005+phillrog@users.noreply.github.com>
2025-07-19 20:29:29 +0000

    k8s inicio

---
+++ k8s/human-gov-infrastructure/terraform/modules/aws_humangov_infrastructure/variables.tf
@@ -0,0 +1,11 @@
+variable "state_name" {
+  description = "The name of the US State"
+}
+
+variable "application_name" {
+  default = "humangov"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
